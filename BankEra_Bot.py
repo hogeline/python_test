8e1c96cb3e002ac2731d1ad3820f7b77a4d9ab24
USER <user@example>
2018-01-25 00:43:05 +0900

    1万円の購入数を追加

--- BankEra_Bot.py
+++ BankEra_Bot.py
@@ -1,15 +1,20 @@
-from coinmarketcap import Market
 import discord
+import urllib.request as req
+
+from coinmarketcap import Market
+from bs4 import BeautifulSoup
+
 
 # 本番token
-token = "NDA1MzY1ODI0NDQyOTkwNTky.DUjV6A.kVeYsW0rldoLX4BtKczQCiXqI58"
+#token = "NDA1MzY1ODI0NDQyOTkwNTky.DUjV6A.kVeYsW0rldoLX4BtKczQCiXqI58"
 # テストtoken
-#token = "NDA0NjE4MDA4MjA0NTQxOTYy.DUoAtQ.DqDyvVDhSIQSMD-KNRtx86WKRgo"
+token = "NDA0NjE4MDA4MjA0NTQxOTYy.DUoAtQ.DqDyvVDhSIQSMD-KNRtx86WKRgo"
 unit_price = 0.019
 correction = 131
 
 client = discord.Client()
 client.get_all_members()
+
 
 @client.event
 async def on_ready():
@@ -23,9 +28,9 @@ async def on_ready():
 async def on_message(message):
     # 送り主がBotだった場合反応したくないので
     if client.user != message.author:
-        if message.content.startswith("?エラリスト") | message.content.startswith("？エラリスト") :
+        if message.content.startswith("?エラリスト") | message.content.startswith("？エラリスト"):
             count = 0
-            for member in client.get_all_members() :
+            for member in client.get_all_members():
                 count += 1
             m = "報告します！ 現在 " + str(count) + "人のエラリストが参加中です！"
             # メッセージが送られてきたチャンネルへメッセージを送ります
@@ -39,8 +44,20 @@ async def on_message(message):
             eur = round(float(coin['price_eur']), 3)
             # バンクエラの価格で割り込み、購入可能なコイン数を算出
             bankera = (eur - correction) / unit_price
-            echo = "1BTCで" + str(round(bankera, 8)) + "コイン購入できます。"
+            echo = "1BTCで約" + str(round(bankera, 8)) + "コイン購入できます。"
 
             await client.send_message(message.channel, echo)
+        elif message.content.startswith("?諭吉") | message.content.startswith("？諭吉"):
+            url = "https://stocks.finance.yahoo.co.jp/stocks/detail/?code=eurjpy"
+            res = req.urlopen(url)
+
+            soup = BeautifulSoup(res, 'html.parser')
+            eur = soup.select_one(".stoksPrice").string
+
+            bankera = 10000 / (float(eur) * unit_price)
+            echo = "1万円で約" + str(round(bankera, 8)) + "コイン購入できます。"
+
+            await client.send_message(message.channel, echo)
+
 
 client.run(token)
